ac0427ee4f686f633bec724b277d94f158a16a51
Jc Denton <besh86@ymail.com>
2015-07-09 11:28:31 +0300

    Indentation fix

--- src/gr/jcdenton/photosphere/PanoramaViewer.java
+++ src/gr/jcdenton/photosphere/PanoramaViewer.java
@@ -16,35 +16,35 @@ import android.util.Log;
 import java.io.File;
 
 public class PanoramaViewer extends Activity implements ConnectionCallbacks, OnConnectionFailedListener {
-
+    
     public static final String TAG = PanoramaViewer.class.getSimpleName();
-
+    
     private GoogleApiClient mClient;
     File file;
-
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mClient = new GoogleApiClient.Builder(this, this, this)
-                .addApi(Panorama.API)
-                .build();
+        .addApi(Panorama.API)
+        .build();
         
         Intent i= getIntent();
         Bundle b = i.getExtras();
         file = new File(b.getString("filepath"));
     }
-
+    
     @Override
     public void onStart() {
         super.onStart();
         mClient.connect();
     }
-
+    
     @Override
     public void onConnected(Bundle connectionHint) {
         Uri uri = Uri.fromFile(file);
         Panorama.PanoramaApi.loadPanoramaInfo(mClient, uri).setResultCallback(
-                new ResultCallback<PanoramaResult>() {
+        new ResultCallback<PanoramaResult>() {
             @Override
             public void onResult(PanoramaResult result) {
                 if (result.getStatus().isSuccess()) {
@@ -59,17 +59,17 @@ public class PanoramaViewer extends Activity implements ConnectionCallbacks, OnC
             }
         });
     }
-
+    
     @Override
     public void onConnectionSuspended(int cause) {
         Log.i(TAG, "connection suspended: " + cause);
     }
-
+    
     @Override
     public void onConnectionFailed(ConnectionResult status) {
         Log.e(TAG, "connection failed: " + status);
     }
-
+    
     @Override
     public void onStop() {
         super.onStop();
